c3389972608ae0db7a6a1fee9d4c89195ab0f547
Harduex <simeon.simka@gmail.com>
2021-10-18 23:27:07 +0300

    New design

---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+// Animations
+AOS.init({
+  anchorPlacement: 'top-left',
+  duration: 1000
+});
+
+window.addEventListener('load', function () {
+  // Add your javascript here
+  function calculateAge(birthday) {
+    // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    var age = Math.abs(ageDate.getUTCFullYear() - 1970);
+    var ele = document.getElementById("years");
+    ele.innerHTML = age;
+  }
+  var birthday = new Date("1999-11-07");
+  calculateAge(birthday);
+
+  emailjs.init("user_PrPI8a8v730dTh7Csb0Bd");
+
+  function sendEmail(e) {
+    e.preventDefault()
+
+    const templateParams = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      message: e.target.message.value,
+    }
+
+    emailjs
+      .send('service_m7y2lg4', 'template_n615z9g', templateParams, 'user_PrPI8a8v730dTh7Csb0Bd')
+      .then(
+        result => {
+          $('.toast').toast('show');
+          $(".form").trigger("reset");
+        },
+        error => {
+          console.log(error.text)
+        }
+      )
+  }
+
+  $('.close-message-toast').on('click', function () {
+    $('.toast').toast('hide');
+  })
+
+  const form = document.querySelector('.form');
+  form.addEventListener('submit', sendEmail);
+
+});
